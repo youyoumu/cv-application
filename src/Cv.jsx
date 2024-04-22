577060a296a909457a6db963989acb88917cbd9e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-22 18:27:18 +0700

    Adjust edit mode toggle

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -5,16 +5,19 @@ function Cv() {
   const [editMode, setEditMode] = useState(false)
 
   return (
-    <div className="bg-base-100 max-w-screen-md mx-auto p-16">
+    <div className="bg-base-100 max-w-screen-md mx-auto p-16 shadow-md">
+      <div className="w-full flex justify-end gap-2">
+        <div>Edit mode</div>
+        <input
+          type="checkbox"
+          className="toggle"
+          checked={editMode}
+          onChange={() => setEditMode(!editMode)}
+        />
+      </div>
       <div>
         <General editMode={editMode} />
       </div>
-      <input
-        type="checkbox"
-        className="toggle"
-        checked={editMode}
-        onChange={() => setEditMode(!editMode)}
-      />
     </div>
   )
 }
