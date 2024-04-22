aa0b46fa0eeb2415178bec2d8e7a63671e86f5a2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-22 13:16:46 +0700

    Setup tailwind

---
+++ src/Cv.jsx
@@ -0,0 +1,9 @@
+function Cv() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button className="">Click me</button>
+    </div>
+  )
+}
+export default Cv
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Cv from './Cv.jsx'
+import './output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Cv />
+  </React.StrictMode>
 )
