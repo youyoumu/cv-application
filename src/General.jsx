0fe9e93edef7ed3ce321bbc48deb5d009f490b1a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-23 11:55:33 +0700

    Add icons to general section

--- src/General.jsx
+++ src/General.jsx
@@ -7,6 +7,18 @@ function General({ editMode }) {
   const [email, setEmail] = useState('example@ex.com')
   const [address, setAddress] = useState('Main Street 123')
 
+  const PhoneIcon = () => {
+    return <span className="material-symbols-outlined mr-2">call</span>
+  }
+
+  const LocationIcon = () => {
+    return <span className="material-symbols-outlined mr-2">location_on</span>
+  }
+
+  const EmailIcon = () => {
+    return <span className="material-symbols-outlined mr-2">mail</span>
+  }
+
   if (editMode) {
     return (
       <div className="mb-8">
@@ -14,13 +26,25 @@ function General({ editMode }) {
           <TextForm text={name} onChangeCallback={setName} />
         </div>
         <div className="grid grid-cols-2 justify-between">
-          <TextForm text={phoneNumber} onChangeCallback={setPhoneNumber} />
+          <TextForm
+            text={phoneNumber}
+            onChangeCallback={setPhoneNumber}
+            Icon={PhoneIcon}
+            parentClassName="mb-2"
+          />
           <TextForm
             className="text-end"
             text={email}
             onChangeCallback={setEmail}
+            Icon={EmailIcon}
+            parentClassName="mb-2"
           />
-          <TextForm text={address} onChangeCallback={setAddress} />
+          <TextForm
+            text={address}
+            onChangeCallback={setAddress}
+            Icon={LocationIcon}
+            parentClassName="mb-2"
+          />
         </div>
       </div>
     )
@@ -28,10 +52,19 @@ function General({ editMode }) {
   return (
     <div className="mb-8">
       <div className="text-5xl font-bold mb-2 py-2">{name}</div>
-      <div className="grid grid-cols-2 justify-between">
-        <div>{phoneNumber}</div>
-        <div className="text-end">{email}</div>
-        <div>{address}</div>
+      <div className="grid grid-cols-2 justify-between gap-2">
+        <div className="flex justify-items-center">
+          <PhoneIcon />
+          {phoneNumber}
+        </div>
+        <div className="flex justify-end justify-items-center">
+          <EmailIcon />
+          {email}
+        </div>
+        <div className="flex justify-items-center">
+          <LocationIcon />
+          {address}
+        </div>
       </div>
     </div>
   )
--- src/TextForm.jsx
+++ src/TextForm.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 
-function TextForm({ text, onChangeCallback, className = '' }) {
+function TextForm({
+  text,
+  onChangeCallback,
+  className = '',
+  Icon = null,
+  parentClassName = ''
+}) {
   const [value, setValue] = useState(text)
 
   return (
-    <input
-      className={`hover:bg-base-200 focus:outline-none w-full ${className}`}
-      type="text"
-      value={value}
-      onChange={(e) => {
-        setValue(e.target.value)
-        onChangeCallback(e.target.value)
-      }}
-    />
+    <div className={`flex justify-items-center ${parentClassName}`}>
+      {Icon && <Icon />}
+      <input
+        className={`hover:bg-base-200 focus:outline-none w-full ${className}`}
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value)
+          onChangeCallback(e.target.value)
+        }}
+      />
+    </div>
   )
 }
 export default TextForm
